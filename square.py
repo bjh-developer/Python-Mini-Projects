c8ad317b8c3cb664e2e8abd5a47e5541c89f64e6
bjh-developer <52413727+bjh-developer@users.noreply.github.com>
2020-03-22 21:28:31 +0800

    Add files via upload

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+
+
+turtle.speed(1)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
